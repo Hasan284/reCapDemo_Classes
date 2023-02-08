4b4589fe2020e466369bc063485502efaefc0c27
Hasan Enes Kaya <86600207+Hasan284@users.noreply.github.com>
2023-02-08 23:08:14 +0300

    Fonksiyonel islem
    Fonksiyonel islem kucuk ornekler

---
+++ reCapDemo_Classes/java/reCapDemo_Classes/Anasayfa.java
@@ -0,0 +1,12 @@
+package reCapDemo_Classes;
+
+public class Anasayfa {
+
+	public static void main(String[] args) {
+		DortIslem dortIslem = new DortIslem();
+	int sonuc=	dortIslem.Topla(5, 120);
+	System.out.println(sonuc);
+
+	}
+
+}
